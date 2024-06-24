0cc753745470a41eb7b53cbe2a73794e7271d086
elias-cj <eliascj.github@gmail.com>
2024-06-24 17:29:29 -0400

    subiendo archivos a github

---
+++ app.js
@@ -0,0 +1,9 @@
+let titulo = document.querySelector('h1');
+titulo.innerHTML = 'juego del numero secreto';
+
+let parrafo = document.querySelector('p');
+parrafo.innerHTML = 'indica un numero del 1 al 10';
+
+function intentoDeUsuario(){
+    alert('click desde el boton');
+}
